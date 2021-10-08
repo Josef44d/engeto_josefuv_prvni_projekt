5ea7115ecf82915fe878f008f16bd0ca67784133
Josef44d <hejnickovi@seznam.cz>
2021-10-08 13:52:24 +0200

    odevzdaný první projekt upavený první projekt ještě jedna úprava doplnění po kontrole

--- projekt01_analyza_textu.py
+++ projekt01_analyza_textu.py
@@ -54,7 +54,7 @@ print(oddelovac)
 
 # zadání čísla textu s podmínkami
 number = input(f'Enter a number btw.1 and {len(TEXTS)} to select: ')
-if not number.isnumeric:
+if not number.isnumeric():
     print('The input is not number')
     quit()
 elif int(number) > len(TEXTS) or int(number) < 1:
@@ -68,35 +68,38 @@ number_index = int(number) - 1
 cista_slova = []
 for slovo in TEXTS[int(number_index)].split():
     cista_slova.append(slovo.strip(",.''()'\n"))
-
+cista_slova1 = []
+for slovo in cista_slova:
+    if slovo.strip(".") != "":
+        cista_slova1.append(slovo)
 # počet slov v textu
-word_count = len(cista_slova)
+word_count = len(cista_slova1)
 print(f'There are {word_count} words in the selected text.')
 
 # počet slov začínající velkým písmenem
 prvni_velke = []
-for slovo in cista_slova:
+for slovo in cista_slova1:
     if slovo.istitle() == True:
         prvni_velke.append(slovo)
 print(f'There are {len(prvni_velke)} titlecase words.')
 
 # počet slov s velkými písmeny
 velka_pismena = []
-for slovo in cista_slova:
+for slovo in cista_slova1:
     if slovo.isupper() and slovo.isalpha() == True:
         velka_pismena.append(slovo)
 print(f'There are {len(velka_pismena)} uppercase words.')
 
 # počet slov s malými písmeny
 mala_pismena = []
-for slovo in cista_slova:
+for slovo in cista_slova1:
     if slovo.islower() and slovo.isalpha() == True:
         mala_pismena.append(slovo)
 print(f'There are {len(mala_pismena)} lowercase words.')
 
 # počet čísel v textu
 cisla = []
-for slovo in cista_slova:
+for slovo in cista_slova1:
     if int(slovo.isnumeric()) == True:
         cisla.append(int(slovo))
 print(f'There are {len(cisla)} numeric strings.')
@@ -109,7 +112,7 @@ print(oddelovac)
 print('LEN' + '|' + '    OCCURENCES     ' + '|' + 'NR.')
 print(oddelovac)
 delka_slov = []
-for s in cista_slova:
+for s in cista_slova1:
     delka_slov.append(int(len(s)));
     delka_slov.sort()
 
